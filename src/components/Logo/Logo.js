bc9f4d7fc94d4e41e0725aa36121d4ee1abca76f
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-02-15 19:24:17 -0800

    Added Logo and Favicon

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,7 +1,7 @@
 
 export const Logo = () => (
   <div className="logo-title">
-      {/* <img src="Logo.png" alt="LocalNewsTV Logo" /> */}
+      <img src='Logo.png' alt="LocalNewsTV Logo" id="Logo"/>
     <h1>LocalNewsTV</h1>
   </div>
 )
